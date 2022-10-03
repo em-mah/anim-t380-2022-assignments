2a0fc4053a473f31f3e682155e7dc7e5c416ee59
em-mah <98603522+em-mah@users.noreply.github.com>
2022-10-03 00:25:37 -0400

    Update cylinder.py
    add maya standalone and argparse

--- assignment-2/python/cylinder.py
+++ assignment-2/python/cylinder.py
@@ -1,1 +1,25 @@
 # testing
+import sys
+import maya.cmds as cmds
+import argparse
+import maya.standalone
+maya.standalone.initialize()
+
+parser = argparse.ArgumentParser(description="This script creates a bunch of cylinders.")
+parser.add_argument('num_cylinders', type=int, help="Number of cylinders")
+
+args = parser.parse_args()
+
+print("Creating {} cylinder(s)...".format(args.num_cylinders))
+
+for i in range(args.num_cylinders):
+    print("Created cylinder #{}".format(i))
+    cmds.cylinder()
+
+print("Meshes in maya scene:")
+print(maya.cmds.ls(geometry=True))
+
+cmds.file (rename="C:/Users/emily/OneDrive/Documents/GitHub/anim-t380-2022-assignments/assignment-2/python/test.ma")
+cmds.file(save=True, type="mayaAscii")
+
+
