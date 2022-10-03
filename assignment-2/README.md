e95096b57d7b6bd432532de7514140b50b22c13b
em-mah <98603522+em-mah@users.noreply.github.com>
2022-10-03 01:42:10 -0400

    Add 2nd arg and update readme

--- assignment-2/README.md
+++ assignment-2/README.md
@@ -1,19 +1,21 @@
 ## **Assignment 2**
  
 ### **Description**
-This script creates a bunch of cylinders.
+This script creates a cylinder pyramid.
 
 ### **Arguments**
 - `num_cylinders` :   Type: int, number of cylinders
+- `radius_start` : Type: int, starting radius
 - `-h, --help` :    Show this help message and exit
 
 ### **Example**
-Run  `mayapy cylinder.py 3` in command prompt. It should print out the following:
+Run  `mayapy cylinder.py 4 2` in command prompt. It should print out the following:
 
-    Creating 3 cylinder(s)...
-    Created cylinder #0
-    Created cylinder #1
-    Created cylinder #2
+    Creating 4 cylinder(s)...
+    Created cylinder #0 with radius 2.0
+    Created cylinder #1 with radius 1.5
+    Created cylinder #2 with radius 1.0
+    Created cylinder #3 with radius 0.5
     Meshes in maya scene:
-    ['nurbsCylinderShape1', 'nurbsCylinderShape2', 'nurbsCylinderShape3']
-3 cylinders will be created in a maya scene and saved in a file called `cylinder.ma`.
+    ['pCylinderShape1', 'pCylinderShape2', 'pCylinderShape3', 'pCylinderShape4']
+4 stacked cylinders will be created in a maya scene and saved in a file called `cylinder.ma`.
--- assignment-2/python/cylinder.py
+++ assignment-2/python/cylinder.py
@@ -1,25 +1,25 @@
-# testing
 import sys
 import maya.cmds as cmds
 import argparse
 import maya.standalone
 maya.standalone.initialize()
 
-parser = argparse.ArgumentParser(description="This script creates a bunch of cylinders.")
-parser.add_argument('num_cylinders', type=int, help="Number of cylinders")
+parser = argparse.ArgumentParser(description="This script creates a cylinder pyramid.")
+parser.add_argument("num_cylinders", type=int, help="Number of cylinders")
+parser.add_argument("radius_start", type=int, help="Starting radius")
 
 args = parser.parse_args()
 
 print("Creating {} cylinder(s)...".format(args.num_cylinders))
 
 for i in range(args.num_cylinders):
-    print("Created cylinder #{}".format(i))
-    cmds.cylinder()
+    n = args.radius_start-(args.radius_start/args.num_cylinders)*i
+    print("Created cylinder #{} with radius {}".format(i,n))
+    cmds.polyCylinder(h=1, r=n)
+    cmds.move(0,0.5+i,0)
 
 print("Meshes in maya scene:")
 print(maya.cmds.ls(geometry=True))
 
-cmds.file (rename="C:/Users/emily/OneDrive/Documents/GitHub/anim-t380-2022-assignments/assignment-2/python/test.ma")
+cmds.file (rename="C:/Users/emily/OneDrive/Documents/GitHub/anim-t380-2022-assignments/assignment-2/python/cylinder.ma")
 cmds.file(save=True, type="mayaAscii")
-
-
