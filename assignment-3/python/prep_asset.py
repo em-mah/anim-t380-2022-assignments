e7b2674ae63172b49c191a2ddf6c0686f2598106
em-mah <98603522+em-mah@users.noreply.github.com>
2022-10-17 17:51:31 -0400

    Update prep_asset.py

--- assignment-3/python/prep_asset.py
+++ assignment-3/python/prep_asset.py
@@ -7,12 +7,13 @@ maya.standalone.initialize()
 
 parser = argparse.ArgumentParser(description="This script creates an empty group with asset name.")
 parser.add_argument("asset_name", type=str, help="Asset name")
+parser.add_argument("output", type=str, help="output")
 
 args = parser.parse_args()
 
 cmds.group(em=True,name = args.asset_name)
 
 savePath = os.getcwd()
-saveFile = savePath + "\\assignment_3.ma"
+saveFile = savePath + args.output
 cmds.file (rename=saveFile)
 cmds.file(save=True, type="mayaAscii")
